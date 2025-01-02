8d4314612e24b8093150eeb364d1fe43e19fce73
Maciej K <maciej.ko1444@gmail.com>
2025-01-02 14:54:57 +0100

    done

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# Task-Tracker-CLI
+# Task-Tracker-CLI with only sys.argv
+
+Project idea from [roadmap.sh backend projects](https://roadmap.sh/projects/task-tracker).
+
+Task Tracker CLI is a simple command-line task manager that allows you to add, update, delete, and view tasks stored in a JSON file.
+
+## Features
+- **Add new tasks** (`add`)
+- **Update task descriptions** (`update`)
+- **Delete tasks** (`delete`)
+- **Mark tasks as "in progress" or "done"** (`mark-in-progress`, `mark-done`)
+- **View task lists** (`list`, `list-done`, `list-in-progress`)
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/maciej3j/task-tracker-cli.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd task-tracker-cli
+   ```
+3. Ensure you have Python 3 installed.
+
+## Usage
+
+Run the program from the command line with the following commands:
+
+### Add a new task
+```bash
+python main.py add "Task description"
+```
+
+### Update a task
+```bash
+python main.py  update TASK_ID "New task description"
+```
+
+### Delete a task
+```bash
+python main.py  delete TASK_ID
+```
+
+### Mark a task as "in progress"
+```bash
+python main.py  mark-in-progress TASK_ID
+```
+
+### Mark a task as "done"
+```bash
+python main.py mark-done TASK_ID
+```
+
+### List tasks
+- All tasks:
+  ```bash
+  python main.py  list
+  ```
+- Tasks by status:
+  ```bash
+  python main.py  list todo
+  python main.py  list in-progress
+  python main.py  list done
+  ```
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-import json
-from datetime import datetime
+from actions import *
+from json import JSONDecodeError
 
 ERROR = '''Usage:
 task-tracker-cli add "something"
@@ -22,126 +21,8 @@ except IndexError:
 try:
     with open("data.json", "r") as f:
         data = json.load(f)
-except FileNotFoundError:
+except (FileNotFoundError, JSONDecodeError):
     data = []
-
-def generate_id(data):
-    if not data:
-        return 1
-    return max(item["ID"] for item in data) + 1
-
-
-def task_add(description, status="todo"):
-    new_task = {
-        "ID": generate_id(data),
-        "description": description,
-        "status": status,
-        "created_at": datetime.now().replace(microsecond=0),
-        "updated_at": datetime.now().replace(microsecond=0)
-    }
-
-    data.append(new_task)
-    with open("data.json", "w") as f:
-        json.dump(data, f,indent=4, default=str)
-        print("Task added successfully.")
-
-
-def task_list():
-    if not data:
-        print("No tasks found.")
-        return
-    if len(sys.argv) == 3:
-        match(sys.argv[2]):
-            case "done":
-                for task in data:
-                    if task['status'] == 'done':
-                        print(f"ID: {task['ID']}, task: {task['description']}, status: {task['status']}")
-            case "todo":
-                for task in data:
-                    if task['status'] == 'todo':
-                        print(f"ID: {task['ID']}, task: {task['description']}, status: {task['status']}")
-
-            case "in-progress":
-                for task in data:
-                    if task['status'] == 'in-progress':
-                        print(f"ID: {task['ID']}, task: {task['description']}, status: {task['status']}")
-
-            case _:
-                print("Wrong option for task status.")
-    elif len(sys.argv) == 2:
-        for task in data:
-            print(f"ID: {task['ID']}, task: {task['description']}, status: {task['status']}, created_at: {task['created_at']}, last_updated: {task['updated_at']}")
-
-def task_update(ID, new_description):
-    global data
-    ID = int(ID)
-    if not data:
-        print("There is nothing to change.")
-        return
-    success = False
-    for task in data:
-        if task['ID'] == ID:
-            task['description'] = new_description
-            task['updated_at'] = datetime.now().replace(microsecond=0)
-            success = True
-    if success:
-        with open("data.json", "w") as f:
-            json.dump(data, f, indent=4, default=str)
-    else:
-        print(f"Task with ID {ID} not found.")
-
-def task_delete(ID):
-    global data
-    ID = int(ID)
-    if not data:
-        print("There is nothing to delete.")
-        return
-    original_length = len(data)
-    updated_data = [item for item in data if item['ID'] != ID]
-    new_length = len(updated_data)
-    if new_length < original_length:
-        print("Task has been successfully deleted.")
-    else:
-        print(f"Not found such task with ID {ID}.")
-
-    with open("data.json", "w") as f:
-        json.dump(updated_data, f, indent=4, default=str)
-
-
-def task_mark_in_progress(ID):
-    global data
-    ID = int(ID)
-    if not data:
-        print("There is nothing to change.")
-        return
-    success = False
-    for task in data:
-        if task['ID'] == ID:
-            task['status'] = "in-progress"
-            success = True
-    if success:
-        with open("data.json", "w") as f:
-            json.dump(data, f, indent=4, default=str)
-    else:
-        print(f"Task with ID {ID} not found.")
-
-
-def task_mark_done(ID):
-    global data
-    ID = int(ID)
-    if not data:
-        print("There is nothing to change.")
-        return
-    success = False
-    for task in data:
-        if task['ID'] == ID:
-            task['status'] = "done"
-            success = True
-    if success:
-        with open("data.json", "w") as f:
-            json.dump(data, f, indent=4, default=str)
-    else:
-        print(f"Task with ID {ID} not found.")
 
 
 match(sys.argv[1]):
